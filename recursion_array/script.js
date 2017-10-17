7c1b2ba3b84b15d7cf8e05b7963d980c25085481
yura.kruhlyk <murove1q@gmail.com>
2017-10-17 21:12:57 +0300

    Completed task recursion array

---
+++ recursion_array/script.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5];
+let i = 0;
+
+function recArr(arr) {
+    console.log(arr[i]);
+    i++;
+    console.log(i);
+    if (i < arr.length) {
+        recArr(arr);
+    }
+}
+
+recArr(arr);
